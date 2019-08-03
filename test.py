7deee9e4fdf3cf00b214719c37f3676e831fe8b3
popcorn9499 <popcorn9499@gmail.com>
2019-08-03 15:38:27 -0500

    test websocket async server

--- test.py
+++ test.py
@@ -1,21 +1,55 @@
-from SimpleWebSocketServer import SimpleWebSocketServer, WebSocket
+# from SimpleWebSocketServer import SimpleWebSocketServer, WebSocket
+# import json
+
+# class SimpleEcho(WebSocket):
+
+#     def handleMessage(self):
+#         # echo message back to client
+#         print(self.data)
+#         #self.sendMessage(self.data)
+
+#     def handleConnected(self):
+#         print(self.address, 'connected')
+#         data = {"Author": "MISTER ME", "Message": "BUT WHY!!", "ServerIcon": "{d}", "Server": "AHH", "Channel": "WHYYY"}
+#         out = json.dumps(data)
+#         self.sendMessage(out)
+
+#     def handleClose(self):
+#         print(self.address, 'closed')
+
+# server = SimpleWebSocketServer('', 8000, SimpleEcho)
+# server.serveforever()
+
+
+import asyncio
+import websockets
 import json
+class websocket():
+    def __init__(self):
+        asyncio.get_event_loop().create_task(self.__asyncInit__())
+        
 
-class SimpleEcho(WebSocket):
+    async def __asyncInit__(self):
+        start_server = websockets.serve(self.hello, "localhost", 8000)
+        await start_server
 
-    def handleMessage(self):
-        # echo message back to client
-        print(self.data)
-        #self.sendMessage(self.data)
+    async def hello(self,websocket, path):
+        name = await websocket.recv()
+        print(type(name))
+        print(f"< {name}")
+        print("    " + str(websocket.remote_address))
+        print("    " + str(websocket.local_address))
+        greeting = f"Hello {name}!"
 
-    def handleConnected(self):
-        print(self.address, 'connected')
-        data = {"Author": "MISTER ME", "Message": "BUT WHY!!"}
+
+
+
+        data = {"Author": "MISTER ME", "Message": "BUT WHY!!", "ServerIcon": "{d}", "Server": "AHH", "Channel": "WHYYY"}
         out = json.dumps(data)
-        self.sendMessage(out)
+        await websocket.send(out)
+        print(f"> {greeting}")
 
-    def handleClose(self):
-        print(self.address, 'closed')
 
-server = SimpleWebSocketServer('', 8000, SimpleEcho)
-server.serveforever()
+w = websocket()
+
+asyncio.get_event_loop().run_forever()
