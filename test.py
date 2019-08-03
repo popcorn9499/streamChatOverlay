2aa1905d61ddfa633e2d73d011a5621248dcd440
popcorn9499 <popcorn9499@gmail.com>
2019-08-02 23:38:44 -0500

    test python code for the websocket

--- test.py
+++ test.py
@@ -1,14 +1,18 @@
 from SimpleWebSocketServer import SimpleWebSocketServer, WebSocket
+import json
 
 class SimpleEcho(WebSocket):
 
     def handleMessage(self):
         # echo message back to client
         print(self.data)
-        self.sendMessage(self.data)
+        #self.sendMessage(self.data)
 
     def handleConnected(self):
         print(self.address, 'connected')
+        data = {"Author": "MISTER ME", "Message": "BUT WHY!!"}
+        out = json.dumps(data)
+        self.sendMessage(out)
 
     def handleClose(self):
         print(self.address, 'closed')
