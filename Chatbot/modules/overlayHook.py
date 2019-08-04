26bcd33a55a409f6a8e091c0f9919888f00569fb
popcorn9499 <popcorn9499@gmail.com>
2019-08-04 10:27:58 -0500

    added placeholders for on connection and on messages for the overlay

--- Chatbot/modules/overlayHook.py
+++ Chatbot/modules/overlayHook.py
@@ -10,3 +10,13 @@ from utils import websocket
 class overlayHook:
     def __init__(self):
         self.w = websocket.server()
+        config.events.onMessage += self.onMessage
+        loop = asyncio.get_event_loop()
+        loop.create_task(self.w.websockConnectAdder(self.onConnect))
+        loop.create_task(self.w.websockReaderAdder(self.onMessage))
+
+    async def onConnect(self,websocket):
+        pass
+
+    async def onMessage(self,message):
+        pass
