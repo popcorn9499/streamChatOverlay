2a7f476e87e4cdd73605fcebe0273bb30da9f590
popcorn <popcorn9499@gmail.com>
2022-05-25 22:43:12 -0500

    idk what this is?

--- test.py
+++ test.py
@@ -27,28 +27,32 @@ import json
 class websocket():
     def __init__(self):
         asyncio.get_event_loop().create_task(self.__asyncInit__())
+        self.websocketList = {}
+        self.websocketReadCallback = []
         
 
     async def __asyncInit__(self):
-        start_server = websockets.serve(self.hello, "localhost", 8000)
+        start_server = websockets.serve(self.connect, "localhost", 8000)
         await start_server
 
-    async def hello(self,websocket, path):
-        name = await websocket.recv()
-        print(type(name))
-        print(f"< {name}")
-        print("    " + str(websocket.remote_address))
-        print("    " + str(websocket.local_address))
-        greeting = f"Hello {name}!"
+    async def connect(self,websocket, path):
+        asyncio.get_event_loop().create_task(self.read(websocket, path))
+        while True:
+            data = {"Author": "MISTER ME", "Message": "BUT WHY!!", "ServerIcon": "{d}", "Server": "AHH", "Channel": "WHYYY"}
+            out = json.dumps(data)
+            await websocket.send(out)
+            await asyncio.sleep(0.05)
 
+    async def read(self,websocket, path):
+        loop = asyncio.get_event_loop()
+        while True:
+            data = await websocket.recv()
+            for callback in self.websocketReadCallback: #handles creating events for when data comes in to handle the data coming in and out
+                loop.create_task(callback(data))
 
-
-
-        data = {"Author": "MISTER ME", "Message": "BUT WHY!!", "ServerIcon": "{d}", "Server": "AHH", "Channel": "WHYYY"}
-        out = json.dumps(data)
-        await websocket.send(out)
-        print(f"> {greeting}")
-
+    async def write(self,websocket, data):
+        data = json.dumps(data)
+        await websocket.send(data)
 
 w = websocket()
 
